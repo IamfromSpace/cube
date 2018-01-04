6369b653fb3c169ad7a18d056a39041d1ec74a3f
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2018-01-03 18:03:23 -0800

    Add cycle methods based on half turns.

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,50 @@ impl CoordCube {
         let mask = !(3 << offset);
         self.orientations_and_corners = self.orientations_and_corners & mask | new;
     }
+    fn cycle_corners_mut(&mut self, cycle: &[(Corner, u64)]) -> () {
+        match cycle.get(0) {
+            Some(&(pi0, oi0)) => {
+                let tmp_p = self.get_corner_p(pi0 as usize);
+                let tmp_o = (self.get_corner_o(pi0 as usize) + oi0) % 3;
+                let mut p_last = pi0;
+                for i in 1..cycle.len() {
+                    let (pi, oi) = cycle[i];
+                    let next_p = self.get_corner_p(pi as usize);
+                    let next_o = (self.get_corner_o(pi as usize) + oi) % 3;
+                    self.set_corner_p_mut(p_last as usize, next_p);
+                    self.set_corner_o_mut(p_last as usize, next_o);
+                    p_last = pi;
+                }
+                self.set_corner_p_mut(p_last as usize, tmp_p);
+                self.set_corner_o_mut(p_last as usize, tmp_o);
+            }
+            None => (),
+        }
+    }
+    fn cycle_edges_mut(&mut self, cycle: &[(Edge, u64)]) -> () {
+        match cycle.get(0) {
+            Some(&(pi0, oi0)) => {
+                let tmp_p = self.get_edge_p(pi0 as usize);
+                let tmp_o = (self.get_edge_o(pi0 as usize) + oi0) % 2;
+                let mut p_last = pi0;
+                for i in 1..cycle.len() {
+                    let (pi, oi) = cycle[i];
+                    let next_p = self.get_edge_p(pi as usize);
+                    let next_o = (self.get_edge_o(pi as usize) + oi) % 2;
+                    self.set_edge_p_mut(p_last as usize, next_p);
+                    self.set_edge_o_mut(p_last as usize, next_o);
+                    p_last = pi;
+                }
+                self.set_edge_p_mut(p_last as usize, tmp_p);
+                self.set_edge_o_mut(p_last as usize, tmp_o);
+            }
+            None => (),
+        }
+    }
+    fn turn_mut(&mut self, turn: HalfTurn) -> () {
+        self.cycle_corners_mut(&half_turn_to_corner_cycle(turn));
+        self.cycle_edges_mut(&half_turn_to_edge_cycle(turn));
+    }
 }
 
 impl From<CubiesCube> for CoordCube {
@@ -164,6 +208,7 @@ impl From<CoordCube> for CubiesCube {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
 enum HalfTurn {
     U,
     UC,
@@ -177,6 +222,46 @@ enum HalfTurn {
     LC,
     R,
     RC,
+}
+
+fn half_turn_to_corner_cycle(turn: HalfTurn) -> [(Corner, u64); 4] {
+    use HalfTurn::*;
+    use Corner::*;
+    match turn {
+        U => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+        UC => [(UFL, 0), (ULB, 0), (UBR, 0), (URF, 0)],
+        D => [(DFR, 0), (DLF, 0), (DBL, 0), (DBL, 0)],
+        DC => [(DBL, 0), (DBL, 0), (DLF, 0), (DFR, 0)],
+        F => [(URF, 1), (UFL, 2), (DLF, 1), (DFR, 2)],
+        FC => [(DFR, 2), (DLF, 1), (UFL, 2), (URF, 1)],
+        //TODO:
+        B => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+        BC => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+        L => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+        LC => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+        R => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+        RC => [(URF, 0), (UBR, 0), (ULB, 0), (UFL, 0)],
+    }
+}
+
+fn half_turn_to_edge_cycle(turn: HalfTurn) -> [(Edge, u64); 4] {
+    use HalfTurn::*;
+    use Edge::*;
+    match turn {
+        U => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        UC => [(UL, 0), (UB, 0), (UR, 0), (UF, 0)],
+        D => [(DF, 0), (DL, 0), (DB, 0), (DR, 0)],
+        DC => [(DR, 0), (DB, 0), (DL, 0), (DF, 0)],
+        //TODO:
+        F => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        FC => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        B => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        BC => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        L => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        LC => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        R => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+        RC => [(UF, 0), (UR, 0), (UB, 0), (UL, 0)],
+    }
 }
 
 fn main() {
