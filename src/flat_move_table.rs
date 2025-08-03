22bead45239ebbafacf862cf6bfa1cafc7642978
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2025-08-02 17:58:56 -0700

    Update our flat_move_table to use our representative_table.

--- src/flat_move_table.rs
+++ src/flat_move_table.rs
@@ -1,139 +1,118 @@
 use permutation_group::PermutationGroup as PG;
 use invertable::Invertable;
 use equivalence_class::EquivalenceClass;
+use representative_table::{ RepresentativeTable, RepIndex, SymIndex };
 
-use std::collections::HashMap;
-use std::hash::Hash;
+use std::sync::Arc;
+use std::convert::TryFrom;
+
+// Opaque type to prevent accidental misuse
+#[derive(Debug, Clone, Copy)]
+pub struct TurnIndex(u8);
 
 #[derive(Debug, Clone)]
-pub struct MoveTable<Perm, Sym, Turn> {
-    representants: Vec<Perm>,
-    table: Vec<(usize, Option<usize>)>,
-    syms: Vec<Sym>,
+pub struct MoveTable<Perm, Sym, PermIndex, Turn> {
+    // TODO: Not even really sure we need this Arc, probably nothing else needs
+    // to access this.  There are some upsides of making this an abstract T (we
+    // don't propagate all the trait bounds), but the downside is that we need
+    // a full trait definition of our RepTable to implement and error messages
+    // get wierder.
+    rep_table: Arc<RepresentativeTable<Perm, Sym, PermIndex>>,
+    table: Vec<(RepIndex<PermIndex>, SymIndex)>,
     turns: Vec<Turn>,
 }
 
-impl<Perm: Hash + Ord + PG + Clone + EquivalenceClass<Sym>, Turn: Copy + Into<Perm>, Sym: Clone> MoveTable<Perm, Sym, Turn> {
-    // TODO: Should we just accept an iteratable type for both Turn and Sym?
-    pub fn new(turns: Vec<Turn>, syms: Vec<Sym>, initial: &Perm) -> Self {
-        let mut representants_hm = HashMap::new();
-        let mut queue = Vec::new();
-        let mut representants_table = Vec::new();
-        // TODO: With capacity hint?
-        let mut table = Vec::new();
+// TODO: Turn: Into<Perm> won't work for patterns (partial permutations).
+// Instead we need some sort of Turnable trait, which can automatically be
+// satisfied if Perm is a PermutationGroup, and Turn is Into<Perm> (which
+// possibly it can implement too).
+impl<Perm: PG + Clone + EquivalenceClass<Sym> + Into<PermIndex>, Turn: Copy + Into<Perm> + PartialEq, Sym: Clone, PermIndex: Copy + Ord + TryFrom<usize> + Into<usize> + Into<Perm>> MoveTable<Perm, Sym, PermIndex, Turn> where <PermIndex as TryFrom<usize>>::Error: std::fmt::Debug {
+    pub fn new(turns: Vec<Turn>, rep_table: Arc<RepresentativeTable<Perm, Sym, PermIndex>>) -> Self {
+        let mut table = Vec::with_capacity(rep_table.len() * turns.len());
 
-        // TODO: Can't we just generate the identity?  Or do we need to accept a list of goal states?
-        representants_hm.insert(initial.clone(), 0);
-        queue.push((initial.clone(), 0));
-        representants_table.push(initial.clone());
-
-        loop {
-            let next = queue.pop();
-
-            match next {
-                None => {
-                  break;
-                }
-                Some((c, rj)) => {
-                    for ti in 0..turns.len() {
-                        let t = turns[ti];
-                        let turned: Perm = c.clone().permute(<Turn as Into<Perm>>::into(t));
-                        let (r, si) = greatest_equivalence(&syms, &turned);
-                        let ri = match representants_hm.get(&r) {
-                            None => {
-                                let current_count = representants_hm.len();
-                                representants_hm.insert(r.clone(), current_count);
-                                queue.push((r.clone(), current_count));
-                                representants_table.push(r);
-                                current_count
-                            },
-                            Some(ri) => *ri,
-                        };
-
-                        let i: usize = rj * turns.len() + ti;
-                        if i >= table.len() {
-                            table.resize(i + 1, (0, None));
-                        }
-                        table[i] = (ri, si)
-                    }
-                }
+        for ri in rep_table.rep_indexes() {
+            let p = rep_table.rep_index_to_perm(ri);
+            for ti in 0..turns.len() {
+                let t = turns[ti];
+                let turned: Perm = p.clone().permute(<Turn as Into<Perm>>::into(t));
+                table.push(rep_table.perm_to_indexes(&turned));
             }
         }
 
         MoveTable {
-            representants: representants_table,
+            rep_table,
             table,
-            syms,
             turns,
         }
     }
-}
 
-fn greatest_equivalence<Perm: Ord + PG + Clone + EquivalenceClass<Sym>, Sym: Clone>(syms: &Vec<Sym>, perm: &Perm) -> (Perm, Option<usize>) {
-    // Identity Sym should not be included in the Sym list (but hypothetically won't hurt anything?)
-    let mut sym = None;
-    let mut greatest = perm.clone();
-    for i in 0..syms.len() {
-        // TODO: This clone is pretty wasteful, because we really only need a
-        // reference, but get_equivalent takes self not &self, because so do
-        // permute and apply.  We can't implement apply on &T, because then
-        // we'd have &T -> &T -> &T, and it's not possible to return a
-        // reference to nothing.  We also can't do something like BorrowMut<T>
-        // -> BorrowMut<T> -> BorrowMut<T>, because we can't return the
-        // original mutable reference, because that would make for two of them.
-        // It seems to me that the functional crate maybe gets it wrong, for
-        // Rust, it should still be &T -> &T -> T, because in functional
-        // programming we never mutate anything!  We should only ever need a
-        // reference to the first two parameters, but then create something new
-        // at the end.
-        let e = perm.clone().get_equivalent(&syms[i]);
-        if e > greatest {
-            greatest = e;
-            sym = Some(i);
-        }
+    pub fn turn_to_turn_index(&self, t: Turn) -> TurnIndex {
+        TurnIndex(self.turns.iter().position(|x| *x == t).unwrap() as u8)
     }
-    ( greatest, sym )
+
+    pub fn turn(&self, ri: RepIndex<PermIndex>, ti: TurnIndex) -> (RepIndex<PermIndex>, SymIndex) {
+        let i = <RepIndex<PermIndex> as Into<usize>>::into(ri) * self.turns.len() + ti.0 as usize;
+        self.table[i]
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::collections::HashSet;
     use two_triangles::*;
 
     #[test]
     fn move_table_is_correct_for_two_triangles_without_symmetry() {
         let turns = vec![Turns::Left, Turns::Right];
         let syms = vec![];
+        let all_perms = (0..120u8).map(|i| i.into());
 
-        let move_table: MoveTable<TwoTriangles, Sym, Turns> = MoveTable::new(turns, syms, &TwoTriangles::identity());
-        let unique_representants = move_table.representants.iter().collect::<HashSet<_>>();
+        let rep_table: RepresentativeTable<TwoTriangles, Sym, u8> = RepresentativeTable::new(syms, all_perms.clone());
+        let rep_table = Arc::new(rep_table);
+        let move_table: MoveTable<TwoTriangles, Sym, u8, Turns> = MoveTable::new(turns.clone(), rep_table.clone());
 
-        // Half of the maximum possible 120 permutations, due to parity
-        assert_eq!(move_table.representants.len(), 60);
-        assert_eq!(move_table.representants.len(), unique_representants.len());
-        // Two moves per representant
-        assert_eq!(move_table.table.len(), 120);
+        // Applying move_table moves is identical to applying permutations
+        for ri in rep_table.rep_indexes() {
+            let p: TwoTriangles = rep_table.rep_index_to_perm(ri);
+            for t in &turns {
+                let by_perm = p.permute((*t).into());
+
+                let ti = move_table.turn_to_turn_index(*t);
+                let (ri, si) = move_table.turn(ri, ti);
+                let before_sym = rep_table.rep_index_to_perm(ri);
+                let by_table = match rep_table.sym_index_to_sym(si) {
+                    None => before_sym,
+                    Some(sym) => before_sym.get_equivalent(&sym),
+                };
+                assert_eq!(by_perm, by_table);
+            }
+        }
     }
 
     #[test]
     fn move_table_is_correct_for_two_triangles_with_rotational_symmetry() {
         let turns = vec![Turns::Left, Turns::Right];
         let syms = vec![Sym::MirrorBoth];
+        let all_perms = (0..120u8).map(|i| i.into());
 
-        let move_table: MoveTable<TwoTriangles, Sym, Turns> = MoveTable::new(turns, syms.clone(), &TwoTriangles::identity());
-        let unique_representants = move_table.representants.iter().collect::<HashSet<_>>();
+        let rep_table: RepresentativeTable<TwoTriangles, Sym, u8> = RepresentativeTable::new(syms, all_perms.clone());
+        let rep_table = Arc::new(rep_table);
+        let move_table: MoveTable<TwoTriangles, Sym, u8, Turns> = MoveTable::new(turns.clone(), rep_table.clone());
 
-        // 32 equivalence classes, based on manual inspection
-        assert_eq!(move_table.representants.len(), 32);
-        assert_eq!(move_table.representants.len(), unique_representants.len());
-        // Two moves per representant
-        assert_eq!(move_table.table.len(), 64);
+        // Applying move_table moves is identical to applying permutations
+        for ri in rep_table.rep_indexes() {
+            let p: TwoTriangles = rep_table.rep_index_to_perm(ri);
+            for t in &turns {
+                let by_perm = p.permute((*t).into());
 
-        for representant in &move_table.representants {
-            for sym in &syms {
-                let equivalent = representant.get_equivalent(sym);
-                assert_eq!(representant == &equivalent || !unique_representants.contains(&equivalent), true);
+                let ti = move_table.turn_to_turn_index(*t);
+                let (ri, si) = move_table.turn(ri, ti);
+                let before_sym = rep_table.rep_index_to_perm(ri);
+                let by_table = match rep_table.sym_index_to_sym(si) {
+                    None => before_sym,
+                    Some(sym) => before_sym.get_equivalent(&sym),
+                };
+                assert_eq!(by_perm, by_table);
             }
         }
     }
@@ -142,20 +121,110 @@ mod tests {
     fn move_table_is_correct_for_two_triangles_with_full_symmetry() {
         let turns = vec![Turns::Left, Turns::LeftPrime, Turns::Right, Turns::RightPrime];
         let syms = vec![Sym::MirrorLR, Sym::MirrorTD, Sym::MirrorBoth];
+        let all_perms = (0..120u8).map(|i| i.into());
 
-        let move_table: MoveTable<TwoTriangles, Sym, Turns> = MoveTable::new(turns, syms.clone(), &TwoTriangles::identity());
-        let unique_representants = move_table.representants.iter().collect::<HashSet<_>>();
+        let rep_table: RepresentativeTable<TwoTriangles, Sym, u8> = RepresentativeTable::new(syms, all_perms.clone());
+        let rep_table = Arc::new(rep_table);
+        let move_table: MoveTable<TwoTriangles, Sym, u8, Turns> = MoveTable::new(turns.clone(), rep_table.clone());
 
-        // 18 equivalence classes, based on manual inspection
-        assert_eq!(move_table.representants.len(), 18);
-        assert_eq!(move_table.representants.len(), unique_representants.len());
-        // Two moves per representant
-        assert_eq!(move_table.table.len(), 72);
+        // Applying move_table moves is identical to applying permutations
+        for ri in rep_table.rep_indexes() {
+            let p: TwoTriangles = rep_table.rep_index_to_perm(ri);
+            for t in &turns {
+                let by_perm = p.permute((*t).into());
 
-        for representant in &move_table.representants {
-            for sym in &syms {
-                let equivalent = representant.get_equivalent(sym);
-                assert_eq!(representant == &equivalent || !unique_representants.contains(&equivalent), true);
+                let ti = move_table.turn_to_turn_index(*t);
+                let (ri, si) = move_table.turn(ri, ti);
+                let before_sym = rep_table.rep_index_to_perm(ri);
+                let by_table = match rep_table.sym_index_to_sym(si) {
+                    None => before_sym,
+                    Some(sym) => before_sym.get_equivalent(&sym),
+                };
+                assert_eq!(by_perm, by_table);
+            }
+        }
+    }
+
+    #[test]
+    fn move_table_is_correct_for_two_triangles_even_parity_without_symmetry() {
+        let turns = vec![Turns::Left, Turns::Right];
+        let syms = vec![];
+        let all_perms = (0..120u8).map(|i| i.into()).filter(|t: &TwoTriangles| t.is_even_parity());
+
+        let rep_table: RepresentativeTable<TwoTriangles, Sym, u8> = RepresentativeTable::new(syms, all_perms.clone());
+        let rep_table = Arc::new(rep_table);
+        let move_table: MoveTable<TwoTriangles, Sym, u8, Turns> = MoveTable::new(turns.clone(), rep_table.clone());
+
+        // Applying move_table moves is identical to applying permutations
+        for ri in rep_table.rep_indexes() {
+            let p: TwoTriangles = rep_table.rep_index_to_perm(ri);
+            for t in &turns {
+                let by_perm = p.permute((*t).into());
+
+                let ti = move_table.turn_to_turn_index(*t);
+                let (ri, si) = move_table.turn(ri, ti);
+                let before_sym = rep_table.rep_index_to_perm(ri);
+                let by_table = match rep_table.sym_index_to_sym(si) {
+                    None => before_sym,
+                    Some(sym) => before_sym.get_equivalent(&sym),
+                };
+                assert_eq!(by_perm, by_table);
+            }
+        }
+    }
+
+    #[test]
+    fn move_table_is_correct_for_two_triangles_even_parity_with_rotational_symmetry() {
+        let turns = vec![Turns::Left, Turns::Right];
+        let syms = vec![Sym::MirrorBoth];
+        let all_perms = (0..120u8).map(|i| i.into()).filter(|t: &TwoTriangles| t.is_even_parity());
+
+        let rep_table: RepresentativeTable<TwoTriangles, Sym, u8> = RepresentativeTable::new(syms, all_perms.clone());
+        let rep_table = Arc::new(rep_table);
+        let move_table: MoveTable<TwoTriangles, Sym, u8, Turns> = MoveTable::new(turns.clone(), rep_table.clone());
+
+        // Applying move_table moves is identical to applying permutations
+        for ri in rep_table.rep_indexes() {
+            let p: TwoTriangles = rep_table.rep_index_to_perm(ri);
+            for t in &turns {
+                let by_perm = p.permute((*t).into());
+
+                let ti = move_table.turn_to_turn_index(*t);
+                let (ri, si) = move_table.turn(ri, ti);
+                let before_sym = rep_table.rep_index_to_perm(ri);
+                let by_table = match rep_table.sym_index_to_sym(si) {
+                    None => before_sym,
+                    Some(sym) => before_sym.get_equivalent(&sym),
+                };
+                assert_eq!(by_perm, by_table);
+            }
+        }
+    }
+
+    #[test]
+    fn move_table_is_correct_for_two_triangles_even_parity_with_full_symmetry() {
+        let turns = vec![Turns::Left, Turns::LeftPrime, Turns::Right, Turns::RightPrime];
+        let syms = vec![Sym::MirrorLR, Sym::MirrorTD, Sym::MirrorBoth];
+        let all_perms = (0..120u8).map(|i| i.into()).filter(|t: &TwoTriangles| t.is_even_parity());
+
+        let rep_table: RepresentativeTable<TwoTriangles, Sym, u8> = RepresentativeTable::new(syms, all_perms.clone());
+        let rep_table = Arc::new(rep_table);
+        let move_table: MoveTable<TwoTriangles, Sym, u8, Turns> = MoveTable::new(turns.clone(), rep_table.clone());
+
+        // Applying move_table moves is identical to applying permutations
+        for ri in rep_table.rep_indexes() {
+            let p: TwoTriangles = rep_table.rep_index_to_perm(ri);
+            for t in &turns {
+                let by_perm = p.permute((*t).into());
+
+                let ti = move_table.turn_to_turn_index(*t);
+                let (ri, si) = move_table.turn(ri, ti);
+                let before_sym = rep_table.rep_index_to_perm(ri);
+                let by_table = match rep_table.sym_index_to_sym(si) {
+                    None => before_sym,
+                    Some(sym) => before_sym.get_equivalent(&sym),
+                };
+                assert_eq!(by_perm, by_table);
             }
         }
     }
